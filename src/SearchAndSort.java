466753c09763be0e4cf1802cb2477a667204a2ca
Kenneth Salanga <ksalanga@ucvts.tec.nj.us>
2019-02-27 14:18:43 -0500

    initial commit

--- src/SearchAndSort.java
+++ src/SearchAndSort.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class SearchAndSort {
 	
 	/**
@@ -7,6 +9,39 @@ public class SearchAndSort {
 	 */
 	
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("What algorithm would you like to execute?");
+		String algorithm = in.nextLine();
+		System.out.println("What type of data?");
+		String type = in.nextLine();
+		System.out.println("How is it stored?");
+		String stored = in.nextLine();
 		
+		
+		
+		switch(algorithm.toLowerCase()) {
+		case "bubble":
+			;
+			break;
+		case "selection":
+			break;
+		case "insertion":
+			break;
+		case "merge":
+			break;
+		case "linear":
+			break;
+		case "binary":
+			break;
+		default:
+			System.out.print("Please select a valid sort!");
+			break;
+		}
+		
+		in.close();
+	}
+	
+	public int[] bubble(int[] n) {
+		return n * bubble(n);
 	}
 }
